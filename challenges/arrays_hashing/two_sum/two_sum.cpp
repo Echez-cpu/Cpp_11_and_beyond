c49a76d7e4e8a79a242a43db02c7c6f0c3680dbd
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2025-09-30 13:41:52 +0200

    Implement twoSum function using unordered_map

--- challenges/arrays_hashing/two_sum/two_sum.cpp
+++ challenges/arrays_hashing/two_sum/two_sum.cpp
@@ -32,3 +32,37 @@ int main() {
     cout << "[" << result[0] << ", " << result[1] << "]" << endl;
     return 0;
 }
+
+
+
+
+// Another way
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> map;
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            if (map.find(complement) != map.end()) {
+                return {map[complement], i};
+            }
+            map[nums[i]] = i;
+        }
+        return {};
+    }
+};
+
+int main() {
+    Solution s;
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    auto result = s.twoSum(nums, target);
+    cout << "[" << result[0] << ", " << result[1] << "]" << endl;
+}
+
