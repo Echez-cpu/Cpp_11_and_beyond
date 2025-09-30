77db71f8fa7409b13ffe9fc8b206fa76aed6f9b3
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2025-09-30 14:56:08 +0200

    Create contains_duplicate.cpp

---
+++ challenges/arrays_hashing/contains_duplicate.cpp
@@ -0,0 +1,58 @@
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+bool containsDuplicate(vector<int>& nums) {
+    unordered_set<int> seen;
+
+    for (size_t i = 0; i < nums.size(); ++i) {
+        int current = nums[i];     
+        if (seen.count(current) > 0) 
+            return true;
+        seen.insert(current);         
+    }
+
+    return false;                     
+}
+
+
+
+
+
+
+
+// Another... Write a main
+
+// class Solution {
+// public:
+//     bool containsDuplicate(vector<int>& nums) {
+
+//         auto it = nums.begin();
+
+//         // auto dup = nums.begin() + 1;
+
+
+//         while(it != nums.end())
+//         {
+//             auto dup = it + 1;
+//             while(dup != nums.end())
+//             {
+//                 if(*it == *dup)
+//                 {
+//                     return true;
+//                 }
+//                 dup++;
+//             }
+//             it++;
+//         }
+        
+//         return false;
+//     }
+// };
+
+
+
+
+
+
+
