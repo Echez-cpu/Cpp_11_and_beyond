045bf4b1ed2451fca484cec6639fed4536c62ebd
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2025-10-07 17:15:01 +0200

    Comment out maxSubArray function and main function

--- challenges/arrays_hashing/Maximum Subarray/Maximum_Subarray.cpp
+++ challenges/arrays_hashing/Maximum Subarray/Maximum_Subarray.cpp
@@ -44,3 +44,53 @@ public:
 
 
 
+
+// using namespace std;
+
+// int maxSubArray(vector<int>& nums) {
+//     int currentSum = nums[0];
+//     int maxSum = nums[0];
+
+//     // Track indices
+//     int start = 0;         // where the current subarray starts
+//     int bestStart = 0;     // start of best subarray
+//     int bestEnd = 0;       // end of best subarray
+
+//     for (size_t i = 1; i < nums.size(); ++i) {
+
+//         // If starting fresh gives a better sum, restart
+//         if (nums[i] > currentSum + nums[i]) {
+//             currentSum = nums[i];
+//             start = i;  // new subarray starts here
+//         } else {
+//             currentSum = currentSum + nums[i]; // continue current subarray
+//         }
+
+//         // Update global max and record indices
+//         if (currentSum > maxSum) {
+//             maxSum = currentSum;
+//             bestStart = start;
+//             bestEnd = i;
+//         }
+//     }
+
+//     // Print the subarray
+//     cout << "Best subarray: [ ";
+//     for (int j = bestStart; j <= bestEnd; ++j)
+//         cout << nums[j] << " ";
+//     cout << "]" << endl;
+
+//     return maxSum;
+// }
+
+// int main() {
+//     vector<int> nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+
+//     int result = maxSubArray(nums);
+//     cout << "Maximum subarray sum: " << result << endl;
+
+//     return 0;
+// }
+
+
+
