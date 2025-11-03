a23f3ef9038ec529420030ce166ee792e002af84
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2025-11-03 11:45:26 +0100

    Comment out alternative maxArea implementation
    Commented out an alternative implementation of the maxArea function.

--- challenges/arrays_hashing/Container_With_Most_Water/Container_With_Most_Water.cpp
+++ challenges/arrays_hashing/Container_With_Most_Water/Container_With_Most_Water.cpp
@@ -25,3 +25,23 @@ public:
         return maxWater;
     }
 };
+
+
+
+/*class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int l = 0;
+        int r = height.size() - 1;
+        int area = 0;
+        while(l <= r) {
+            int calculatedArea = min(height[l], height[r]) * (r - l);
+            area = max(area, calculatedArea);
+
+            if(height[l] < height[r]) l++;
+            else r--;
+        }
+
+        return area;
+    }
+};*/
